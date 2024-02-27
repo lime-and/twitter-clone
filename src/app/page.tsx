1af874f4e7ddd43d85cb2e3baf6e886acb348edf
lime <michaelliman21@gmail.com>
2024-02-27 10:25:01 +0900

    finished sidebuttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { BiHomeCircle, BiUser } from "react-icons/bi";
-import {BsBell, BsBookmark} from "react-icons/bs";
+import {BsBell, BsBookmark, BsEnvelope, BsThreeDots} from "react-icons/bs";
 import { HiOutlineHashtag } from "react-icons/hi";
 import { HiEnvelope } from "react-icons/hi2";
 import { BsTwitter } from "react-icons/bs";
@@ -27,7 +27,7 @@ const NAVIGATION_ITEMS = [
   },
   {
     title: "Messages",
-    icon: HiEnvelope
+    icon: BsEnvelope
   },
   {
     title: "Profile",
@@ -40,7 +40,8 @@ const Home = () => {
     <div className="w-full h-full flex justify-center items-center relative bg-black">
       <div className="max-w-screen-lg w-full h-full flex relative">
         {/* Left sidebar for navigation/header */}
-        <section className="fixed w-72 flex flex-col h-screen space-y-4 my-4">
+        <section className="fixed w-[275px] flex flex-col items-stretch h-screen">
+          <div className="flex flex-col items-stretch h-full space-y-4 mt-4">
           {NAVIGATION_ITEMS.map((item) => (
             <Link
               className="hover:bg-white/10 text-2xl transition durration-200 flex items-center justify-start w-fit space-x-4
@@ -54,9 +55,24 @@ const Home = () => {
               {item.title !== "X" && <div>{item.title}</div>}
             </Link>
           ))}
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <button className="rounded-full rounded-full bg-primary p-4 text-2xl text-center hover:bg-opacity-70 transition
+          duration-200">
             Post
           </button>
+          </div>
+          <button className="rounded-full flex items-center space-x-2 rounded-full bg-transparent p-4 text-2xl text-center hover:bg-white/20 transition
+          duration-200 w-full">
+            <div className="flex item-center space-x-2">
+              <div className="rounded-full bg-slate-400 w-8 h-8"></div>
+              <div className="text-left text-sm" >
+                <div className="font-semibold">Club Of Coders</div>
+                <div className="text-xs">@clubasdf</div>
+            </div>
+            </div>
+            <div>
+              <BsThreeDots />
+            </div>
+            </button>
         </section>
         {/*<main>Home timeline</main>
         <section>right section</section>*/}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,3 +20,5 @@ const config: Config = {
   },
   plugins: [],
 };
+
+export default config;
