599fdfd132309ca26a19283ad0ffc41eb7bc3da4
lime <michaelliman21@gmail.com>
2024-07-15 07:00:37 +0900

    right side update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ const Home = () => {
       <div className="max-w-[72vw] w-full h-full flex relative border-1">
         <LeftSidebar />
         <MainComponent />
-        <section className="w-[full] sticky top-2 right- mt-2 flex flex-col items-stretch h-screen px-6">
+        <section className="w-[full] sticky top-2 mt-2 flex flex-col items-stretch h-screen px-6">
           <div>
             <div className="relative w-full h-full group">
               <input
@@ -40,17 +40,17 @@ const Home = () => {
           <div className="flex flex-col rounded-xl bg-neutral-950 my-4">
             <h3 className="font-bold text-xl my-2 px-4">Who to follow</h3>
             <div>
-              {Array.from({ length: 4 }).map((_, i) => (
-                <div key={i} 
-                  className="hover:bg-white/10 p-4 flex  space-x-4 items-center last:rounded-b-x transition-duration-200"
-                  >
-                  <div className="w-10 h-10 bg-neutral-600 rounded-full"></div>
-                  <div className="flex flex-col 2">
-                    <div>Other user</div>
-                    <div>@user123</div>
+              {Array.from({ length: 3 }).map((_, i) => (
+                <div key={i} className="hover:bg-white/10 flex p-4 justify-between items-center last:rounded-b-x transition-duration-200">
+                  <div className="flex items-center space-x-2">
+                    <div className="w-10 h-10 bg-neutral-600 rounded-full"></div>
+                    <div className="flex flex-col">
+                      <div className="font-bold text-white">Other user</div>
+                      <div className="text-gray-500 text-xs">@user123</div>
+                    </div>
                   </div>
                   <div>
-                    <button className="rounded-full px-6 py-2 bg-white text-white text-neutral-950">
+                    <button className="rounded-full px-6 py-2 bg-white text-neutral-950">
                       Follow
                     </button>
                   </div>
